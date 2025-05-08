eda10d15a6d9d41f0f803524cbd35e1b479fd751
Freya Raja <freyaraja135@gmail.com>
2025-05-08 22:12:02 +0530

    Link to repository

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="w-full py-4 text-center text-lg text-white">
       <h1 className="underline">
         
-        <Link href="https://github.com/kautilyadevaraj/SchedulingAlgorithmSimulator" target="_blank">
+        <Link href="https://github.com/Freya135/Scheduling-Algorithm-Simulator" target="_blank">
           View Github Repository
         </Link>
       </h1>
